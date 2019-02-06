20fcf4ae9e1605ac74ef961f7fa098e009f4e0fb
augustuszzq <44146265+augustuszzq@users.noreply.github.com>
2019-02-06 21:01:06 +0000

    Add files via upload

--- udp/UDPServer.java
+++ udp/UDPServer.java
@@ -1,6 +1,6 @@
 /*
- * Created on 01-Mar-2016
- */
+* Created on 01-Mar-2016
+*/
 package udp;
 
 import java.io.IOException;
@@ -13,13 +13,16 @@ import java.nio.charset.StandardCharsets;
 import common.MessageInfo;
 
 public class UDPServer {
+
 	private DatagramSocket recvSoc;
-	private int totalMessages = -1;
+	private static int totalMessages = -1;
 	//private boolean[] receivedMessages;
-	private int[] checklist;
+	private static int[] checklist;
 	private boolean close=false;
 	private int count=0;
 	private int count_process = 0;
+	private static int misscounter=0;
+
 	private void run() {
 		int				pacSize;
 		byte[]			pacData;
@@ -36,38 +39,42 @@ public class UDPServer {
 				//System.out.println("xxx");
 				processMessage(pacdata_string);
 
+
 			}catch(Exception e){
 				System.out.println("Exception"+e.getMessage());
 				count++;
 			}
+
 		}
-		// TO-DO: Receive the messages and process them by calling processMessage(...).
-		//        Use a timeout (e.g. 30 secs) to ensure the program doesn't block forever
-	}
-
-	public void processMessage(String data) {
-		MessageInfo msg = null;
-		try{
-			msg = new MessageInfo(data);
-			if(totalMessages==-1){
-				totalMessages = msg.totalMessages;
-				checklist = new int[totalMessages];
-
-				//receivedMessages = new boolean[totalMessages];
-				count_process = totalMessages;
-			}
-			System.out.printf("%d",msg.messageNum);
-			System.out.printf("\n");
-			//receivedMessages[msg.messageNum]=true;
-			checklist[msg.messageNum]=1; //set to 1 if transmitted, for checking later
-
-			count_process--;
-			if (count_process==0) {
-				close = true;
-			}
-		}catch(Exception e){
-			System.err.println("ignore "+e.getMessage());
+			// TO-DO: Receive the messages and process them by calling processMessage(...).
+			//        Use a timeout (e.g. 30 secs) to ensure the program doesn't block forever
 		}
+
+		public void processMessage(String data) {
+			MessageInfo msg = null;
+			try{
+				msg = new MessageInfo(data);
+				if(totalMessages==-1){
+					totalMessages = msg.totalMessages;
+
+
+					checklist = new int[totalMessages];
+
+					//receivedMessages = new boolean[totalMessages];
+					count_process = totalMessages;
+				}
+				System.out.printf("%d",msg.messageNum);
+				System.out.printf("\n");
+				//receivedMessages[msg.messageNum]=true;
+				checklist[msg.messageNum]=1; //set to 1 if transmitted, for checking later
+
+				count_process--;
+				if (count_process==0) {
+					close = true;
+				}
+			}catch(Exception e){
+				System.err.println("ignore "+e.getMessage());
+			}
 		}
 		// TO-DO: Use the data to construct a new MessageInfo object
 		// TO-DO: On receipt of first message, initialise the receive buffer
@@ -76,14 +83,14 @@ public class UDPServer {
 		//        any missing messages
 
 
-	public UDPServer(int rp) {
-		try{
+		public UDPServer(int rp) {
+			try{
 				recvSoc = new DatagramSocket(rp);
 				recvSoc.setSoTimeout(30000);
-		}catch(SocketException e){System.out.println("Socket:"+e.getMessage());
+			}catch(SocketException e){System.out.println("Socket:"+e.getMessage());
+		}
+		System.out.println("UDPServer ready");
 	}
-	System.out.println("UDPServer ready");
-}
 	public static void main(String args[]) {
 		int	recvPort;
 		// Get the parameters from command line
@@ -97,9 +104,22 @@ public class UDPServer {
 		recvPort = Integer.parseInt(args[0]);
 		UDPServer server = new UDPServer(recvPort);
 		server.run();
+		for (int temp=0;temp<totalMessages;temp++){
+			if(checklist[temp]==0){
+				
+				System.out.printf("\nMissed info = %d",temp);
+				misscounter=misscounter+1;
 
-		// TO-DO: Construct Server object and start it by calling run().
+			}
+
+		}
+
+		System.out.printf("\ntotal message missed = %d",misscounter);
+
 	}
+
+	// TO-DO: Construct Server object and start it by calling run().
+
 
 
 }
